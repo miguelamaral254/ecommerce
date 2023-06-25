3213074dfa557e2095cf05a7e103e5032bae3e7b
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-06-25 09:06:14 -0300

    att
    add mongoDB, some pages

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+
+export default function Home() {
+  const {data : session} = useSession()
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b> 
+      </h2> 
+      <div className="flex gap-1 bg-gray-300 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image}  alt='img' className="w-6- h-6"/>
+        <span className="px-2"></span>
+        {session?.user?.name}
+      </div>
+    </div>
+  </Layout>
+}
