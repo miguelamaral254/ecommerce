7ac78c6eca7ee3091c160df8a9d9182a9605f731
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-06-29 01:55:37 -0300

    Update
    Add [
    Images upload
    draggable images
    
    ]

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,36 +1,97 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Spinner from "./Spinner";
+import {ReactSortable} from "react-sortablejs";
 
 export default function ProductForm({
+    _id,
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
+    images:existingImages,
+    
 }) {
     const [title, setTitle] = useState(existingTitle || '')
     const [description,setDescription] = useState(existingDescription || '')
     const [price, setPrice] = useState(existingPrice || '')
+    const [images,setImages] = useState(existingImages || [])
     const [goToProducts,setGoToProducts] = useState(false)
+    const [isUploading, setIsUploading] = useState(false)
     const router = useRouter()
-    async function createProduct(ev){
+    async function saveProduct(ev){
         ev.preventDefault()
-        const data = {title ,description ,price}
-        await axios.post('/api/products', data)
-        setGoToProducts(true)
-    }
+        const data = {title ,description ,price, images}
+        if (_id) {
+            //update
+            await axios.put('/api/products', {...data, _id})
+          } else {
+            //create
+            await axios.post('/api/products', data)
+          }
+          setGoToProducts(true)
+        }
+        
+
     if (goToProducts) {
        router.push('/products')
     }
+    async function uploadImages(ev) {
+        const files = ev.target?.files
+        if (files?.length > 0) {
+            setIsUploading(true)
+            const data = new FormData()
+            for (const file of files) {
+                data.append('file', file)
+            }
+            const res = await axios.post('/api/upload', data,)
+            setImages(oldImages => {
+                return [...oldImages,...res.data.links]
+            })
+            setIsUploading(false)
+        }
+    }
+    function updateImagesOrder(images) {
+        setImages(images);
+    }
     return(
                    
-            <form onSubmit={createProduct}>
-                <h1>New Product</h1>
+            <form onSubmit={saveProduct}>                
                 <label>Product name</label>
                 <input type="text"
                 placeholder="Product name"
                 value={title}
                 onChange={ev => setTitle(ev.target.value)}
                 />
+                <label>
+                    Photos
+                </label>
+                <div className="mb-2 flex flex-wrap gap-1">
+                <ReactSortable
+                    list={images}
+                    className="flex flex-wrap gap-1"
+                    setList={updateImagesOrder}>
+                    {!!images?.length && images.map(link => (
+                    <div key={link} className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200">
+                        <img src={link} alt="" className="rounded-lg"/>
+                    </div>
+                    ))}
+                </ReactSortable>
+                    {isUploading && (
+                        <div className="h-24 p-1 flex items-center">
+                            <Spinner/>
+                        </div>
+                    )}
+                    <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                        <div>Upload</div>
+                        <input type="file" 
+                        onChange={uploadImages}
+                        className="hidden"/>
+                        </label>                                            
+                </div>
                 <label>Description</label>
                 <textarea
                 placeholder="description"
--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,8 @@ import { model, models, Schema }  from "mongoose"
 const ProductSchema = new Schema({
     title:{type:String, required:true},
     description: String,
-    price: {type:Number, required:true}
+    price: {type:Number, required:true},
+    images: [{type:String}],
 })
 
 
